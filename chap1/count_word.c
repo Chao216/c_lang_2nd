c4ff0582d1b5c879e455a4124afbdc4c2011e222
co.jiaan <co.jiaan@proton.me>
2023-10-10 23:56:35 +0800

    count word

---
+++ chap1/count_word.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define IN 1
+#define OUT 0
+/*we have defined 2 constant above*/
+
+void main()
+{
+
+int c, nl, nw, nc, state;
+
+state = OUT;
+/*first inital state is OUT side a word*/
+
+nl = nc = nw = 0;
+
+while ((c = getchar()) != EOF) {
+	++nc;
+	/* increase char count*/
+	if (c == "\n") {
+		++nl;
+		/* change line, nl increase*/
+	}
+	if (c == " " || c == "\t" || c == "\n") {
+		/* if new char is space, \t, \n means cursor out of a word we need change state to OUT*/
+		state = OUT;
+	}
+
+
+	else if (state == OUT) {
+		/*change state back to IN, and increase number of word*/
+		state = IN;
+		++nw;
+	}
+
+}
+	printf("number of line: %d\tnumber of word: %d\tnumber of char: %d",nl,nw,nc);
+}
