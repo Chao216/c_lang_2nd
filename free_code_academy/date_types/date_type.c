06b8f74247a47087343d43b4d467f461e4283831
co.jiaan <co.jiaan@proton.me>
2023-10-25 07:47:29 +0800

    printf placeholder

---
+++ free_code_academy/date_types/date_type.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int age = 26;
+
+    double gpa = 3.67;
+
+    printf("the age is %d",age);
+    printf("gpa is %d",gpa);
+
+
+
+    return 0;
+}
