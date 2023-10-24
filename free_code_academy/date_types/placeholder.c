584f51ad5439cd9959a873764cb19c4073c66178
co.jiaan <co.jiaan@proton.me>
2023-10-25 07:53:47 +0800

    printf place holder

---
+++ free_code_academy/date_types/placeholder.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char c1 = 'A';
+    char c2[] = "Bella";
+    int i1 = 10;
+    float f1 = 20.7;
+    double pi = 3.1415;
+
+    printf("i1 = %d\n",i1);
+    printf("f1 = %f\n",f1);
+    printf("pi = %lf\n",pi);
+    printf("c1 = %c\n",c1);
+    printf("c2 = %s\n",c2);
+}
