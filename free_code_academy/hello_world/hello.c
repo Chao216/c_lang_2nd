d8aa300a0b06ccc6f67695665bb78685af2af15b
co.jiaan <co.jiaan@proton.me>
2023-10-24 22:24:00 +0800

    print more

--- free_code_academy/hello_world/hello.c
+++ free_code_academy/hello_world/hello.c
@@ -2,5 +2,10 @@
 
 int main(){
     printf("Hello world!\n");
+    printf("/ |\n");
+    printf("/  |\n");
+    printf("/   |\n");
+    printf("/    |\n");
+    printf("/_____|\n");
     return 0;
 }
