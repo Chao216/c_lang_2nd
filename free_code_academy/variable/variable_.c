570b5ef7e976c201ac465b4efc91056cceaef03e
co.jiaan <co.jiaan@proton.me>
2023-10-24 22:38:08 +0800

    use variable to store values

---
+++ free_code_academy/variable/variable_.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char characterName[] = "John";
+    int characterAge = 26;
+    printf("Once, there was a man named %s\n",characterName);
+    printf("and %s is now %d years old\n",characterName,characterAge);
+    printf("He like his name:  %s\n",characterName);
+    printf("but he is not happy for being %d years old\n",characterAge);
+
+
+
+    return 0;
+}
