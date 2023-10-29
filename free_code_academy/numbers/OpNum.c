571b8b462ace34aa2ad889915cec84aaae98762a
co.jiaan <co.jiaan@proton.me>
2023-10-29 23:24:05 +0800

    #include <math.h>

---
+++ free_code_academy/numbers/OpNum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("%f\n",sqrt(10));
+    printf("%f\n",pow(10,3));
+    printf("%f\n",floor(10.3241));
+    printf("%f\n",ceil(10.3343));
+    return 0;
+}
