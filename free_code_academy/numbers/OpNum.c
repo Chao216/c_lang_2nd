5582c00a1f14b1d5284768c55cd1f9ead9733b77
co.jiaan <co.jiaan@proton.me>
2023-10-29 23:30:15 +0800

    // or /* */

--- free_code_academy/numbers/OpNum.c
+++ free_code_academy/numbers/OpNum.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-
+// this is a single line of comment
 int main(){
     printf("%f\n",sqrt(10));
     printf("%f\n",pow(10,3));
     printf("%f\n",floor(10.3241));
     printf("%f\n",ceil(10.3343));
+    /*
+    while here 
+    you can write 
+    multi lines of comments
+    as you need!
+    */
     return 0;
 }
