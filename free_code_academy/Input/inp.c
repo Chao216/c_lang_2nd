ee3ad1c275f2e0f167343d55b5b62109e3d5fb72
co.jiaan <co.jiaan@proton.me>
2023-10-30 23:48:25 +0800

    fgets to get a line

--- free_code_academy/Input/inp.c
+++ free_code_academy/Input/inp.c
@@ -4,6 +4,7 @@
 int main(){
     char name[20];
     int age;
+    char girl[20];
 
     printf("what is your name: ");
     scanf("%s",&name);
@@ -11,6 +12,12 @@ int main(){
     scanf("%d",&age);
     printf("%s is %d years old\n",name,age);
 
+    printf("who is you favorite actress: ");
+    getchar(); // change to a new line
+    fgets(girl,20,stdin); // get multiple lines
+    printf("You favorite girl is %s.\n",girl);
+
+    
     return 0;
 
 
