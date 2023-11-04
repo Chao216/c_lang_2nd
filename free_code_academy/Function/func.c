5f5bc7b4c3244fdae7eb0210abbd2c6a58d4232a
co.jiaan <co.jiaan@proton.me>
2023-11-04 15:15:01 +0800

    auto commit @11/04/23 15:15:01

---
+++ free_code_academy/Function/func.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sayHi();// you have to declare first before main function
+int main(){
+
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+    sayHi();
+
+
+}
+
+void sayHi(){
+    printf("Hi!\n");
+}
