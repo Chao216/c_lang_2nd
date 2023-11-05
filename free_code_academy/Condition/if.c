c44171ec5c1a229ff0b33da234cf88583156a4c2
co.jiaan <co.jiaan@proton.me>
2023-11-05 16:35:58 +0800

    if else if else

--- free_code_academy/Condition/if.c
+++ free_code_academy/Condition/if.c
@@ -10,9 +10,23 @@ int getBig(int n1, int n2){
     return n2;
 }
 
+int Max(int a, int b, int c){
+
+    if (a >= b && a >= c){
+        return a;
+    } else if (b >= a && b >= c)
+    {
+        return b;   /* code */
+    }
+
+    return c;
+    
+}
+
 int main(){
 
     printf("%d\n",getBig(10,3));
+    printf("%d\n",Max(10,10086,3));
 
     return 0;
 }
