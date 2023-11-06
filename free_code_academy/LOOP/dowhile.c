368cbefcdd52852619510712f3872adb8b75dfe6
co.jiaan <co.jiaan@proton.me>
2023-11-06 23:19:54 +0800

    do while

---
+++ free_code_academy/LOOP/dowhile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int height = 173;
+
+    do {
+        printf("I love you\n");
+        printf("%d\n",height);
+
+    } while (height >= 180);
+
+// difference is do while will execute atleast onece before test condition
+
+    return 0;
+}
