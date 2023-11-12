9244677695bad3160e98da8ea09032961def18b1
co.jiaan <co.jiaan@proton.me>
2023-11-12 20:13:46 +0800

    auto commit @11/12/23 20:13:46

--- free_code_academy/Return/ret.c
+++ free_code_academy/Return/ret.c
@@ -5,8 +5,18 @@ double triple(double d1){
     return d1 *3;
 }
 
+char * combName(char first[], char last[]){
+    return ;
+}
+
+
 int main(){
 
     printf("%lf\n",triple(3.14159));
+    
+
+    printf("%s\n",combName("Tony","Emily"));
+
     return 0;
+
 }
